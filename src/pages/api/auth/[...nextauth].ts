66132903ec25f27d71845591ceee28d133d8e458
Felipe Damasceno <felipe@superempresa.com.br>
2021-10-19 20:30:01 -0300

    auth faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -9,6 +11,39 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile){
+      const {email} = user
+      
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('users_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'), 
+              { data: {email} }
+            ),
+            q.Get( // Select
+              q.Match(
+                q.Index('users_by_email'), // no fauna não consegue buscar informações sem indice
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch(e) {
+        return false
+      }
+    }
+  }
 })
