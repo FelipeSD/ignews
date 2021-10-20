188594196d0582bfbc6678b92e2d8012ce199c70
Felipe Damasceno <felipe@superempresa.com.br>
2021-10-19 21:20:45 -0300

    stripe checkout session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,8 +8,30 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({priceId}: SubscribeButtonProps){
+    async function handleSubscribe(){
+        const [session] = useSession();
+
+        if(!session){
+            signIn('github');
+            return;
+        }
+
+        // criação da checkout session
+        try {
+            const response = await api.post('/subscribe');
+            const { sessionId } = response.data as { sessionId: string };
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({
+                sessionId,
+            });
+        } catch(err) {
+            console.error(err.message);
+        }
+    }
     return (
         <button
+            onClick={handleSubscribe}
             className={styles.subscribeButton}
             type="button"
         >
