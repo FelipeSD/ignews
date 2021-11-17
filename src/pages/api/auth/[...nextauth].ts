dde8aef7c70de601e63d49babc645e7c7e868e9c
Felipe Damasceno <felipesdam@outlook.com>
2021-11-17 08:48:34 -0300

    pagina de posts com prismic

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,6 +13,41 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session(session){
+      try{
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  'ref',
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),  
+                "active"
+              )
+            ])
+          )
+        )
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        }
+      }catch(e){
+        return {
+          ...session,
+          activeSubscription: null
+        }
+      }
+    },
     async signIn(user, account, profile){
       const {email} = user
       
@@ -22,7 +57,7 @@ export default NextAuth({
             q.Not(
               q.Exists(
                 q.Match(
-                  q.Index('users_by_email'),
+                  q.Index('user_by_email'),
                   q.Casefold(user.email)
                 )
               )
@@ -33,7 +68,7 @@ export default NextAuth({
             ),
             q.Get( // Select
               q.Match(
-                q.Index('users_by_email'), // no fauna não consegue buscar informações sem indice
+                q.Index('user_by_email'), // no fauna não consegue buscar informações sem indice
                 q.Casefold(user.email)
               )
             )
