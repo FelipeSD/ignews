f28d39d031ebf9c759dc464a7cbceefca6ba35b7
Felipe Damasceno <felipesdam@outlook.com>
2022-02-24 20:49:35 -0300

    Update env variable prod.

--- src/pages/api/_lib/menageSubscription.ts
+++ src/pages/api/_lib/menageSubscription.ts
@@ -4,50 +4,54 @@ import {fauna} from "../../../services/fauna";
 import {stripe} from '../../../services/stripe';
 
 export async function saveSubscription(subscriptionId: string, customerId: string, createAction = false) {
-    // Buscar o usuário no banco do Fauna com o ID
-    const userRef = await fauna.query(
-        q.Select(
-            'ref',
-            q.Get(
-                q.Match(
-                    q.Index("user_by_stripe_customer_id"),
-                    customerId
+    try{
+        // Buscar o usuário no banco do Fauna com o ID
+        const userRef = await fauna.query(
+            q.Select(
+                'ref',
+                q.Get(
+                    q.Match(
+                        q.Index("user_by_stripe_customer_id"),
+                        customerId
+                    )
                 )
             )
         )
-    )
-
-    // salvar os dados da subscription no Fauna
-    const subscription = await stripe.subscriptions.retrieve(subscriptionId)
-
-    const subscriptionData = {
-        id: subscription.id,
-        userId: userRef,
-        status: subscription.status,
-        price_id: subscription.items.data[0].price.id,
-    }
-
-    if (createAction) {
-        await fauna.query(
-            q.Create(
-                q.Collection("subscriptions"),
-                {data: subscriptionData}
+    
+        // salvar os dados da subscription no Fauna
+        const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+    
+        const subscriptionData = {
+            id: subscription.id,
+            userId: userRef,
+            status: subscription.status,
+            price_id: subscription.items.data[0].price.id,
+        }
+    
+        if (createAction) {
+            await fauna.query(
+                q.Create(
+                    q.Collection("subscriptions"),
+                    {data: subscriptionData}
+                )
             )
-        )
-    } else {
-        await fauna.query(
-            q.Replace(
-                q.Select(
-                    "ref",
-                    q.Get(
-                        q.Match(
-                            q.Index("subscription_by_id"),
-                            subscriptionId
+        } else {
+            await fauna.query(
+                q.Replace(
+                    q.Select(
+                        "ref",
+                        q.Get(
+                            q.Match(
+                                q.Index("subscription_by_id"),
+                                subscriptionId
+                            )
                         )
-                    )
-                ),
-                {data: subscriptionData}
+                    ),
+                    {data: subscriptionData}
+                )
             )
-        )
+        }
+    }catch(e){
+        throw new Error(e)
     }
 }
